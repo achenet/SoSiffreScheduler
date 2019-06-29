c346b10ff344ed86329fcf085c87bb31a0f0d541
achenet <apchenet@gmail.com>
2019-06-29 14:05:08 +0200

    Saturday is Caturday :3

---
+++ guillermo.py
@@ -0,0 +1,10 @@
+#simple "how does gui work" test kit
+
+import tkinter
+#we create the window
+w = tkinter.Tk()
+w.mainloop()
+
+
+#okay, now we gunna have to figure out how to put like actual information in this
+#and finally, make it easy to run on different platforms, possibly on phones. 
