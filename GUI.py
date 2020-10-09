d8537348649ce0ed8471ab2a138ffe2fef98b6be
achenet <apchenet@gmail.com>
2020-10-09 13:48:44 +0200

    still trying to resolve attribute error to make text field work

--- GUI.py
+++ GUI.py
@@ -10,18 +10,28 @@ class Window(tk.Tk):
         super().__init__()
         self.title("Scheduler")
         self.geometry("800x600")
+        self.initialize_window()
+
+    def initialize_window(self):
+
         self.quit_button = tk.Button(self,  text = "Quit", command = self.quit)
-        self.quit_button.grid()
+        self.quit_button.grid(row=2, column = 2)
         self.entry = tk.Entry(self)
-        self.entry.grid()
-        self.text = ""
-        self.enter_text_button = tk.Button(self.entry, text = "enter", command =self.entry.read_entry())
-        self.enter_text.grid()
+        self.entry.grid(row = 1, column = 1)
+        self.show_text = ""
+        self.enter_text_button = tk.Button(self, text = "enter", command =self.read_entry())
+        self.enter_text_button.grid()
         self.enter_text_label = tk.Label(self.entry, text = "Employee Name")
-
+        self.enter_text_label.grid()
+        self.output = tk.Text(self) 
+        self.output.grid()
+    
+    
+    
     def read_entry(self):
-        self.text = self.entry.get()
-
+        self.show_text = self.entry.get()
+        
+        self.output.insert(tk.END,self.show_text)
     
 
 
