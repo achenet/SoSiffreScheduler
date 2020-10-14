2afc78f3232ced6f3fe32e77deb874047a5d416c
achenet <apchenet@gmail.com>
2020-10-14 12:53:41 +0200

    redid GUI using mutliple frames

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
 #this is where we try and make the GUI
 
-import Logic
+import Logic as lo
 
 import tkinter as tk
 
@@ -13,28 +13,49 @@ class Window(tk.Tk):
     def initialize_window(self):
         self.title("Scheduler")
         self.frame1 = tk.Frame(self)
-        self.quit_button = tk.Button(self,  text = "Quit", command = self.quit)
-        self.entry = tk.Entry(self.frame1)
         self.frame1.grid(row = 0, column = 0)
+        self.quit_button = tk.Button(self,  text = "Quit", command = self.quit)
         self.quit_button.grid(row=2, column = 2)
-        self.entry.grid(row = 1)
-        self.show_text = ""
-        self.output = tk.Text(self.frame1, height = 2, width = 50) 
-        self.output.grid(row = 3)
-        self.enter_text_button = tk.Button(self.frame1, text = "enter", command =self.read_entry)
-        self.enter_text_button.grid(row = 2)
-        self.enter_text_label = tk.Label(self.frame1, text = "Employee Name")
-        self.enter_text_label.grid(row = 0)
+        self.add_worker_button = tk.Button(self.frame1, text = "Add Employee", command = self.add_worker)
+        self.add_worker_button.grid(row = 2, column = 1) 
+        self.back_to_start_button = tk.Button(self, text = "Back to Start", command = self.back_to_start)
+        self.add_employee_frame = tk.Frame(self)
+        self.add_employee_label = tk.Label(self.add_employee_frame, text = "Please Fill in Employee details")
+        self.add_employee_label.grid()
+        self.employee_name_frame = tk.LabelFrame(self.add_employee_frame, text = "Employee Name")
+        self.employee_name_entry = tk.Entry(self.employee_name_frame)
+        self.employee_name_frame.grid(row = 1, column = 0)
+        self.employee_name_entry.grid()
+        self.employee_hours_frame = tk.LabelFrame(self.add_employee_frame, text = "Contract Hours")
+        self.employee_hours_entry = tk.Entry(self.employee_hours_frame)
+        self.employee_hours_frame.grid(row = 1, column = 1)
+        self.employee_hours_entry.grid()
+        self.enter_employee_data_button = tk.Button(self.add_employee_frame, text = "Enter Worker Data", command = self.enter_worker_data)
+        self.enter_employee_data_button.grid(row = 1, column = 2)        
 
-    
-    
-    
-    def read_entry(self):
-        self.show_text = self.entry.get()
-        
-        self.output.insert(tk.END," " + self.show_text)
-    
 
+    def add_worker(self):
+        self.frame1.grid_remove()
+        self.add_employee_frame.grid(row = 0)
+        self.back_to_start_button.grid(row = 2)
+
+    def enter_worker_data(self):
+        self.worker_data_frame = tk.Frame(self)
+        name = self.employee_name_entry.get()
+        contract = self.employee_hours_entry.get()
+        worker = lo.Worker(name,contract,[])
+        self.worker_data_display = tk.Label(self.worker_data_frame, text = worker)
+        self.worker_data_display.grid(row = 0)
+        self.add_employee_frame.grid_remove()
+        self.worker_data_frame.grid(row = 0)
+
+
+    def back_to_start(self):
+        for frame in self.grid_slaves():
+            frame.grid_remove()
+        self.frame1.grid()
+        self.quit_button.grid(row = 2, column = 2)
+        self.back_to_start_button.grid_remove()
 
 
 if __name__ == '__main__':
